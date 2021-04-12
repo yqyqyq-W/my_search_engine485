fe49c52c028243b0ae23d02fe7d70bc496584a0a
dewdewde <45724009+dewdewde@users.noreply.github.com>
2021-04-12 22:37:26 +0800

    0.1

--- search/search/views/index.py
+++ search/search/views/index.py
@@ -9,20 +9,21 @@ import requests
 
 @search.app.route('/', methods=['GET'])
 def result():
-    query = request.form['q']
-    weight = request.form['w']
-    connection = search.search.model.get_db()
-    res = requests.get('http://localhost:8000/api/v1/hits/?w=' + weight + '&q=' + query)
-    index = 0
-    content = {'results': [], 'ifempty': 0}
-    for item in res:
-        if index > 10:
-            break
-        cur = connection.execute(
-            "SELECT * FROM Documents WHERE docid = ?", (item['docid'],)
-        )
-        content['results'].append(cur.fetchall()[0])
-        index = index + 1
-    if len(content['results']) == 0:
-        content['ifempty'] = 1
+    content = {'results': [], 'ifempty': 1}
+    if bool(request.args):
+        query = request.args['q']
+        weight = request.args['w']
+        connection = search.search.model.get_db()
+        res = requests.get('http://localhost:8000/api/v1/hits/?w=' + weight + '&q=' + query).json()
+        index = 0
+        for item in res:
+            if index > 10:
+                break
+            cur = connection.execute(
+                "SELECT * FROM Documents WHERE docid = ?", (item['docid'],)
+            )
+            content['results'].append(cur.fetchall()[0])
+            index = index + 1
+        if len(content['results']) != 0:
+            content['ifempty'] = 0
     return flask.render_template("index.html", **content)
